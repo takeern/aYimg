283d97838926ae2c8dfddacfbe3df82c396fab8e
tangqi <tangqi@bilibili.com>
2018-09-07 15:37:25 +0800

    [TEST]

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import init from './lib/init'
 //proccing
 import grayScaleFn from './lib/grayScale/index'
 import binary from './lib/binary/binary'
+// import fiter from './lib/fiter/index'
 // import { erosive, dilate } from './lib/binary/erosive'
 // import sharpen from './lib/sharpening'
 
@@ -48,17 +49,35 @@ pro.then(() => {
   clothes(grayScaleFn, 'weight')
 })
 .then(() => {
-  clothes(binary, 'otsu')
+  clothes(binary, 'average')
 })
 
 // .then(() => {
-//   clothes(dilate, 1)
+//   clothes(erosive, 1)
+// })
+// .then(() => {
+//   clothes(erosive, 1)
+// })
+// .then(() => {
+//   clothes(erosive, 1)
 // })
 // .then(() => {
 //   clothes(dilate, 1)
 // })
 // .then(() => {
 //   clothes(dilate, 1)
+// })
+// .then(() => {
+//   clothes(fiter, 'knnA')
+// })
+// .then(() => {
+//   clothes(fiter, 'knnA')
+// })
+// .then(() => {
+//   clothes(fiter, 'knnA')
+// })
+// .then(() => {
+//   clothes(fiter, 'knnA')
 // })
 // .then(() => {
 //   clothes(sharpen, 'roberts')
@@ -85,22 +104,23 @@ const pick = function (showPiex) {
 // 2 change data handle  delete handledata
 
 // test
-const arr = [ 1, 3, 2, 3, 2, 1, 2, 1, 4, 3, 1, 10, 2, 3, 4, 5, 2, 6, 18, 8, 5, 5, 7, 0, 8 ]
+// const arr = [ 1, 3, 2, 3, 2, 1, 2, 1, 4, 3, 1, 10, 2, 3, 4, 5, 2, 6, 18, 8, 5, 5, 7, 0, 8 ]
+// const arr = [2,1,4,3,3, 10,2,3,4,4, 2,6,18,8,7 ,5,7,0,8,8 ,6,7,8,9,9]
+// const arr = [1,3,2,3,2,1,2 ,1,2,1,4,3,3,2, 1,10,2,3,4,4,2, 5,2,6,18,8,7,3, 5,5,7,0,8,8,5, 5,6,7,8,9,9,8, 4,5,6,8,8,6,7]
 // const op = []
 // for (let i = 0; i < arr.length; i++) {
 //   const a = new Array(4).fill(arr[i])
 //   op.push(...a)
 // }
-// debug('123', op)
-// op.width = 6
+// debug('123', arr)
+// op.width = 7
 // const tsData = {
-//   width: 6,
+//   width: 7,
 //   data: op,
 // }
-// const data1 = erosive(tsData, 4)
+// const data1 = fiter(tsData, 'knn')
 // const copyData = []
 // for (let i = 0; i < data1.length; i+=4) {
 //   copyData.push(data1[i]) 
 // }
-// debug('copydata', copyData)
-
+// debug('fiterData', copyData)
