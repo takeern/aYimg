272ee300c3d487f7e3e37985ba794a2ec62af3a8
tangqi <tangqi@bilibili.com>
2018-09-07 15:34:50 +0800

    [ADD] add fiter function

--- src/lib/fiter/fiter.js
+++ src/lib/fiter/fiter.js
@@ -4,11 +4,21 @@ export default (copyData, imageData, code) => {
   const { data, width } = imageData
   const { length } = data
   const start = (width + 1) * 4
+  const checkCode = []
+  for (let x = -2; x < 3; x ++) {
+    for(let y = 0; y < 3; y++) {
+      if(y === 0 && x <= 0) continue
+      checkCode.push({
+        x,
+        y,
+      })
+    }
+  }
   switch (code) {
   case 'avg' : {
     for (let i = start; i < length; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const sum = copyData[i - (width + 1) * 4] + copyData[i - 4 * width] + copyData[i - 4 * (width - 1)] + copyData[i - 4] + copyData[i] + copyData[i + 4] + copyData[i + 4 * (width - 1)] + copyData[i + 4 * width] + copyData[i + 4 * (width + 1)]
         const operator = sum / 9
         data [i] = operator
@@ -20,8 +30,8 @@ export default (copyData, imageData, code) => {
   }
   case 'median' : {
     for (let i = start; i < length; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const arr = [ copyData[i - (width + 1) * 4], copyData[i - 4 * width], copyData[i - 4 * (width - 1)], copyData[i - 4], copyData[i], copyData[i + 4], copyData[i + 4 * (width - 1)], copyData[i + 4 * width], copyData[i + 4 * (width + 1)] ]
         arr.sort(byAsc)
         const operator = arr[4]
@@ -32,13 +42,46 @@ export default (copyData, imageData, code) => {
     }
     break
   }
-  case 'knn' : {
-    for (let i = start; i < length; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
-        const arr = [ copyData[i - (width + 1) * 4] - copyData[i], copyData[i - 4 * width] - copyData[i], copyData[i - 4 * (width - 1)] - copyData[i], copyData[i - 4] - copyData[i], copyData[i + 4] - copyData[i], copyData[i + 4 * (width - 1)] - copyData[i], copyData[i + 4 * width] - copyData[i], copyData[i + 4 * (width + 1)] - copyData[i] ]
+  case 'knnA' : {
+    const start = (2 * width + 1) * 4 
+    const end = length - width * 4 * 2
+    for (let i = start; i < end; i += 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1) && x !== 1 && x !== (width - 2)) {
+        const arr = []
+        for (let code of checkCode) {
+          const first = copyData[i - 4 * (width * code.y + code.x)] - copyData[i]
+          const ser = copyData[i + 4 * (width * code.y + code.x)] - copyData[i]
+          // if (Math.abs(first) > Math.abs(ser)) arr.push(ser)
+          // else arr.push(first)
+          arr.push(first)
+          arr.push(ser)
+        }
         arr.sort(absByAsc)
-        const operator = (arr[0] + arr[1] + arr[2] + arr[3] + arr[4]) / 5 + copyData[i]
+        const operator = arrSum(arr.slice(0, 8)) / 9 + copyData[i]
+        data [i] = operator
+        data [i + 1] = operator
+        data [i + 2] = operator
+      }
+    }
+    break
+  }
+  case 'knnM' : {
+    const start = (2 * width + 1) * 4 
+    const end = length - width * 4 * 2
+    for (let i = start; i < end; i += 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1) && x !== 1 && x !== (width - 2)) {
+        const arr = []
+        for (let code of checkCode) {
+          const first = copyData[i - 4 * (width * code.y + code.x)] - copyData[i]
+          const ser = copyData[i + 4 * (width * code.y + code.x)] - copyData[i]
+          if (Math.abs(first) > Math.abs(ser)) arr.push(ser)
+          else arr.push(first)
+        }
+        arr.sort(absByAsc)
+        const slice = arr.slice(0, 8)
+        const operator = slice[4] + copyData[i]
         data [i] = operator
         data [i + 1] = operator
         data [i + 2] = operator
@@ -48,19 +91,10 @@ export default (copyData, imageData, code) => {
   }
   case 'snn' : {
     const start = (2 * width + 1) * 4 
-    const checkCode = []
-    for (let x = -2; x < 3; x ++) {
-      for(let y = 0; y < 3; y++) {
-        if(y === 0 && x <= 0) continue
-        checkCode.push({
-          x,
-          y,
-        })
-      }
-    }
-    for (let i = start; i < length; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4 && x !== 1 && x !== (width - 2)) {
+    const end = length - width * 4 * 2
+    for (let i = start; i < end; i += 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1) && x !== 1 && x !== (width - 2)) {
         const arr = []
         for (let code of checkCode) {
           const first = copyData[i - 4 * (width * code.y + code.x)] - copyData[i]
--- src/lib/fiter/index.js
+++ src/lib/fiter/index.js
@@ -1,11 +1,12 @@
 import { isImageData, copyImageData } from '../ulit'
 import fiter from './fiter'
-// const debug = require('debug')('aYimg:fiter')
+const debug = require('debug')('aYimg:fiter')
 
 export default (imageData, code) => {
   if(!isImageData(imageData)) throw new Error('get imageData error from fiter')
   const { data } = imageData
   const copyData = copyImageData(data)
+  debug(copyData)
   const logData = fiter(copyData, imageData, code)
   return logData
 }
