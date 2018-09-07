c89cc7b96b1708a92809c7354396087a67004757
tangqi <tangqi@bilibili.com>
2018-09-07 15:35:34 +0800

    [FIX] fix x outside

--- src/lib/sharpening/index.js
+++ src/lib/sharpening/index.js
@@ -10,8 +10,8 @@ export default (imageData, code) => {
   switch (code) {
   case 'priwitt' : {
     for(let i = width * 4; i < len; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const Gx = copyData[i + 4 * (width - 1)] + copyData[i + 4 * width] + copyData[i + 4 * (width + 1)] - copyData[i - 4 * (width + 1)] - copyData[i - 4 * width] - copyData[i - 4 * (width - 1)]
         const Gy = copyData[i - 4 * (width - 1)] + copyData[i + 4] + copyData[i + 4 * (width + 1)] - copyData[i - 4 * (width + 1)] - copyData[i - 4] - copyData[i + 4 * (width - 1)]
         const operator = Math.round(Math.sqrt(Math.pow(Gx, 2)+ Math.pow(Gy, 2)))
@@ -24,8 +24,8 @@ export default (imageData, code) => {
   }
   case 'sobel' : {
     for(let i = width * 4; i < len; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const Gx = copyData[i + 4 * (width - 1)] + 2 * copyData[i + 4 * width] + copyData[i + 4 * (width + 1)] - copyData[i - 4 * (width + 1)] - 2 * copyData[i - width * 4] - copyData[i - 4 * (width - 1)]
         const Gy = copyData[i + 4 * (1 - width)] + 2 * copyData[i + 4] + copyData[i + 4 * (width + 1)] - copyData[i - 4 * (width + 1)] - 2 * copyData[i - 4] - copyData[i + 4 * (width - 1)]
         const operator = Math.round(Math.sqrt(Math.pow(Gx, 2)+ Math.pow(Gy, 2)))
@@ -38,8 +38,8 @@ export default (imageData, code) => {
   }
   case 'roberts' : {
     for(let i = width * 4; i < len; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const operator = Math.abs(copyData[i] - copyData[i + 4 * (width + 1)]) + Math.abs(copyData[i + 4] - copyData[i + 4 * width])
         data [i] = operator
         data [i + 1] = operator
@@ -50,8 +50,8 @@ export default (imageData, code) => {
   }
   case 'wallis' : {
     for(let i = width * 4; i < len; i += 4) {
-      const x = i % (width * 4)
-      if(x !== 0 && x !== (width - 1) * 4) {
+      const x = (i / 4) % width
+      if(x !== 0 && x !== (width - 1)) {
         const operator = 46 * (4 * log10(copyData[i] + 1) - [ log10(copyData[i - 4] + 1) + log10(copyData[i + 4] + 1) + log10(copyData[i - 4 * width]+ 1) + log10(copyData[i + 4 * width] + 1) ]) // eslint-disable-line
         data [i] = operator 
         data [i + 1] = operator 
